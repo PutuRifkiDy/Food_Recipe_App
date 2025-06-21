1917049e5c6255dc00799a1c31b6ecd55fcaac84
ceditt <aditgus87@gmail.com>
2025-06-21 22:45:45 +0800

    add landing page, icon app dan update splash screen

--- app/src/main/java/com/example/foodrecipeapp/LandingPageActivity.kt
+++ app/src/main/java/com/example/foodrecipeapp/LandingPageActivity.kt
@@ -1,32 +1,20 @@
 package com.example.foodrecipeapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class SplashActivity : AppCompatActivity() {
+class LandingPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_landing_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        Handler(Looper.getMainLooper()).postDelayed({
-            goToLandingPageActivity()
-        }, 3000L)
-    }
-    private fun goToLandingPageActivity(){
-        Intent(this, LandingPageActivity::class.java).also {
-            startActivity(it)
-            finish()
         }
     }
 }
