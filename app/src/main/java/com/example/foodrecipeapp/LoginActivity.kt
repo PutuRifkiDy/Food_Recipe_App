ed183a7159143bc847969b886df9c02c4709659d
PutuRifkiDy <puturifki56@gmail.com>
2025-06-22 17:05:16 +0800

    added: intent to redirect login or reister, membuat table user, recipe dan category, membuat fungsi insertUser dan readUser

--- app/src/main/java/com/example/foodrecipeapp/LoginActivity.kt
+++ app/src/main/java/com/example/foodrecipeapp/LoginActivity.kt
@@ -1,20 +1,60 @@
 package com.example.foodrecipeapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.foodrecipeapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    // untuk akses component ui yang ada di xml layoutnya
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var databaseHelper: DatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // akses file xml ke kotlin supaya ga pake findViewById Lagi
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        // declare database helper
+        databaseHelper = DatabaseHelper(this)
+
+        binding.loginButton.setOnClickListener{
+            val loginEmail = binding.loginEmail.text.toString()
+            val loginPassword = binding.LoginPassword.text.toString()
+
+            loginDatabase(loginEmail, loginPassword)
+        }
+
+        binding.signupRedirect.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private fun loginDatabase(email: String, password: String) {
+        val userExists = databaseHelper.readUser(email, password)
+        if (userExists) {
+            Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
         }
     }
 }
