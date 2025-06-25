ba7d9999d654d0a2d850a1c125a27e0a1bb30e98
Bujawan22 <tarabujawan222403@gmail.com>
2025-06-25 21:24:34 +0800

    update detail technique and add detail recipe

--- app/src/main/java/com/example/foodrecipeapp/CookingTechniquesActivity.kt
+++ app/src/main/java/com/example/foodrecipeapp/CookingTechniquesActivity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class CookingTechniquesActicity : AppCompatActivity() {
+class CookingTechniquesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_cooking_techniques_acticity)
+        setContentView(R.layout.activity_cooking_techniques_activity)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
