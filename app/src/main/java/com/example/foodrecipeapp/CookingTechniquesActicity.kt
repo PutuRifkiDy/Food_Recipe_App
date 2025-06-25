fe9dcb5a371ecc74ffb70834c6048229ec02aca0
Bujawan22 <tarabujawan222403@gmail.com>
2025-06-25 18:15:14 +0800

    add detail cooking techniques

---
+++ app/src/main/java/com/example/foodrecipeapp/CookingTechniquesActicity.kt
@@ -0,0 +1,20 @@
+package com.example.foodrecipeapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CookingTechniquesActicity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_cooking_techniques_acticity)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
