a2fa3c0d7382a2ebb905ba5d9cbdecfc32257e7e
PutuRifkiDy <puturifki56@gmail.com>
2025-07-02 23:36:09 +0800

    add: intent dari edit profile activity ke profile fragment

--- app/src/main/java/com/example/foodrecipeapp/EditProfileActivity.kt
+++ app/src/main/java/com/example/foodrecipeapp/EditProfileActivity.kt
@@ -1,20 +1,33 @@
 package com.example.foodrecipeapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.foodrecipeapp.databinding.ActivityEditprofileBinding
 
 class EditProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditprofileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_editprofile)
+
+        binding = ActivityEditprofileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.backButtonEditProfile.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("navigate_to", "profile")
+            startActivity(intent)
+            finish()
         }
     }
 }
--- app/src/main/java/com/example/foodrecipeapp/MainActivity.kt
+++ app/src/main/java/com/example/foodrecipeapp/MainActivity.kt
@@ -22,13 +22,21 @@ class MainActivity : AppCompatActivity() {
 
         enableEdgeToEdge()
         setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        replaceLayoutToFragment(HomeFragment())
+        val destination = intent.getStringExtra("navigate_to")
+        if (destination == "profile") {
+            replaceLayoutToFragment(ProfileFragment())
+            binding.bottomNavigationView.selectedItemId = R.id.profile
+        } else {
+            replaceLayoutToFragment(HomeFragment())
+        }
+
         binding.bottomNavigationView.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.home -> replaceLayoutToFragment(HomeFragment())
