42aee0eeb0f63c159dd37a94f6af5f431ca0cc83
PutuRifkiDy <puturifki56@gmail.com>
2025-07-04 21:20:59 +0800

    added: update profile information gender

--- app/src/main/java/com/example/foodrecipeapp/EditProfileActivity.kt
+++ app/src/main/java/com/example/foodrecipeapp/EditProfileActivity.kt
@@ -41,27 +41,41 @@ class EditProfileActivity : AppCompatActivity() {
             val user = dbHelper.getUserDataByEmail(email)
 
             if (user != null) {
+                binding.name.setText(user.name)
                 binding.about.setText(user.about)
                 binding.dob.setText(user.birthday)
                 binding.number.setText(user.phone)
                 binding.email.setText(user.email)
-
+                when (user.gender) {
+                    "Female" -> binding.genderGroup.check(R.id.female)
+                    "Male" -> binding.genderGroup.check(R.id.male)
+                }
             }
         }
 
         binding.saveButton.setOnClickListener{
+            val newName = binding.name.text.toString()
             val newAbout = binding.about.text.toString()
             val newBirthday = binding.dob.text.toString()
             val newPhone = binding.number.text.toString()
             val newEmail = binding.email.text.toString()
 
+            val selectedGenderId = binding.genderGroup.checkedRadioButtonId
+            val selectedGender = when (selectedGenderId) {
+                R.id.female -> "Female"
+                R.id.male -> "Male"
+                else -> ""
+            }
+
             if (email != null) {
                 val dbHelper = DatabaseHelper(this)
                 val values = ContentValues().apply {
+                    put("name", newName)
                     put("about", newAbout)
                     put("birthday", newBirthday)
                     put("phone_number", newPhone)
                     put("email", newEmail)
+                    put("gender", selectedGender)
                 }
 
                 val db = dbHelper.writableDatabase
@@ -69,10 +83,12 @@ class EditProfileActivity : AppCompatActivity() {
 
                 // update di sharedPreferencesnya juga
                 val editor = sharePrev.edit()
+                editor.putString("user_name", newName)
                 editor.putString("user_about", newAbout)
                 editor.putString("user_birthday", newBirthday)
                 editor.putString("phone_number", newPhone)
                 editor.putString("user_email", newEmail)
+                editor.putString("user_gender", selectedGender)
                 editor.apply()
 
                 Toast.makeText(this, "Profile updated successfully", Toast.LENGTH_SHORT).show()
