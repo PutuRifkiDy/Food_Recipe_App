a368300f6aada42675c4ce91e5f6e9b0f9586801
PutuRifkiDy <puturifki56@gmail.com>
2025-07-09 18:46:09 +0800

    added: CRUD cooking technique dan logout admin

---
+++ app/src/main/java/com/example/foodrecipeapp/model/CookingTechnique.kt
@@ -0,0 +1,9 @@
+package com.example.foodrecipeapp.model
+
+data class CookingTechnique(
+    val id: Int,
+    val title: String,
+    val imagePath: String,
+    val description: String,
+    val method: String
+)
